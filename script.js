3dcd3ed5e78b36f51caf4a168d2789c6cc56cf87
Selvesflowers <timurpetrov110@gmail.com>
2025-11-06 19:42:21 +0100

    Website have been done

---
+++ script.js
@@ -0,0 +1,25 @@
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close");
+
+openBtn.onclick = () => modal.style.display = "flex";
+closeBtn.onclick = () => modal.style.display = "none";
+window.onclick = (e) => {
+  if (e.target === modal) modal.style.display = "none";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
